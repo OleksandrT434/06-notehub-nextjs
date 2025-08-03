84b84c8b482196ab2096017bec497e88e9be26e5
Aleksandr Tkachenko <prosto.batman1234@gmail.com>
2025-08-03 19:29:23 +0200

    upa

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,6 +2,8 @@ import AppPage from "./notes.client";
 import { fetchNotes } from "@/lib/api";
 import type { PaginatedNotes } from "@/lib/api";
 
+
+
 export default async function NotesPage() {
   const initialData: PaginatedNotes = await fetchNotes(1, 12, "");
   return <AppPage initialData={initialData} />;
