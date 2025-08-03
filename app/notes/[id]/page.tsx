bc5eef4bc8830cb32d68102c37cafd1b738de444
Aleksandr Tkachenko <prosto.batman1234@gmail.com>
2025-08-03 17:54:47 +0200

    last

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -3,11 +3,11 @@ import { fetchNoteById } from '@/lib/api';
 import NoteDetails from "../NoteDetails.client";
 
 type Props = {
-  params: Promise<{ id: string }>;
+  params: { id: string };
 };
 
 export default async function NoteDetailsPage({ params }: Props) {
-  const { id } = await params
+  const { id } = params
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
